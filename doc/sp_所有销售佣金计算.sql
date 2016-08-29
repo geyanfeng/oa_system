c98fa0b1232a9050f3cfe62d86bd7fdd3ba598f9
frank tan <frank.tan8@live.com>
2016-08-29 05:40:57 +0000

    sp_所有销售佣金计算.sql edited online with Bitbucket

--- doc/sp_所有销售佣金计算.sql
+++ doc/sp_所有销售佣金计算.sql
@@ -17,9 +17,25 @@ repeat
 		set @current_Date = CURRENT_DATE;
 		set @year = year(@Current_Date);
 		set @quarter = quarter(@Current_Date);
+    set @month = month(@Current_Date);
+    set @day = day(@Current_Date);
     call calcuate_saler_quarter_commission(salerid,@year,@quarter);
-		#计算前一个季度佣金
+		#当前季度的第一天计算前一个季度佣金
+    IF (@month = 1 OR @month = 4 OR @month = 7 OR @month = 10) AND @day = 1 THEN
+        IF @quarter = 1 THEN
+           set @previous_year = @year - 1;
+					 set @previous_quarter = 4;
+	      ELSE
+					 set @previous_year = @year;
+					 set @previous_quarter = @quarter - 1;
+				END IF;
+        call calcuate_saler_quarter_commission(salerid,@previous_year,@previous_quarter);
+    END IF;
+		
 until done end repeat;  
 close cur_saler;  
 
 end
+
+
+
